c08b13e8d1d31efd0fa6dc53a41354985674507e
leocapeccir <leocapeccir@gmail.com>
2026-03-23 12:01:55 -0300

    feat: new feat responsive

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
 AOS.init();
+
+const images = document.querySelectorAll('.carousel-track img');
+const nextBtn = document.querySelector('.right-arrow');
+const prevBtn = document.querySelector('.left-arrow');
+
+let index = 0;
+
+function updateCarousel() {
+  images.forEach(img => {
+    img.classList.remove('active', 'left', 'right');
+    img.style.opacity = "0";
+  });
+
+  const prev = (index - 1 + images.length) % images.length;
+  const next = (index + 1) % images.length;
+
+  images[index].classList.add('active');
+  images[prev].classList.add('left');
+  images[next].classList.add('right');
+
+  images[index].style.opacity = "1";
+  images[prev].style.opacity = "1";
+  images[next].style.opacity = "1";
+}
+
+/* clique nas imagens */
+images.forEach((img, i) => {
+  img.addEventListener('click', () => {
+    index = i;
+    updateCarousel();
+  });
+});
+
+/* setas */
+nextBtn.addEventListener('click', () => {
+  index = (index + 1) % images.length;
+  updateCarousel();
+});
+
+prevBtn.addEventListener('click', () => {
+  index = (index - 1 + images.length) % images.length;
+  updateCarousel();
+});
+
+/* inicia */
+updateCarousel();
